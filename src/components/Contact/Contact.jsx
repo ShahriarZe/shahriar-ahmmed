2fb997b26fcbf18f96e3be01c096a9e8398e4835
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-03-17 23:15:18 +0600

    Hire section added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <h1>Email</h1>
             <h1>Download Resume</h1>
             <h1>Email</h1>
-            <h1>Download CV</h1>
+            <h1>Hire Me</h1>
         </div>
     );
 };
